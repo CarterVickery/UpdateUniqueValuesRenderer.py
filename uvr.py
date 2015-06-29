8ccab8686dd233a2b110bc932ea631c5097af425
Carter Vickery <Carter.Vickery@wakegov.com>
2015-06-29 11:58:36 -0400

    Update uvr.py

--- uvr.py
+++ uvr.py
@@ -1,7 +1,7 @@
 # vcsRevalUniqueValueSymbologyUpdate.py
-# Created on: 2015-05-21
+# Created on: 2015-05-13
 # Author: Carter Vickery
-# Description: Python script to update the Unique Value Symbology for the VCS Reval Layer used by RMaps and Restart the Map Service to pick up the new values
+# Description: Python script to update the Unique Value Symbology for the VCS Reval Layer used by an Internal Application
 # ---------------------------------------------------------------------------
 
 # Import arcpy module
@@ -9,9 +9,21 @@ import arcpy
 
 mxd = arcpy.mapping.MapDocument("D:\\Workspace\\Python\\VCSReval.mxd")
 df = arcpy.mapping.ListDataFrames(mxd, "Layers")[0]
-RevalVCSlyr = arcpy.mapping.ListLayers (mxd, "VCS Reval",df)[0]
+RevalVCSlyr = arcpy.mapping.ListLayers (mxd, "VCS Reval",df)
 
-RevalVCSlyr.symbology.addAllValues()
+for lyr in arcpy.mapping.ListLayers(mxd,"",df):
+    if lyr.name == "VCS Reval":
+       lyr.symbology.addAllValues()
 arcpy.RefreshActiveView()
 arcpy.RefreshTOC()
 mxd.save()
+
+del mxd
+del RevalVCSlyr
+
+	
+# Write messages to a Text File
+txtFile = open("D:\\Workspace\\Python\\vcsRevalUniqueValueSymbologyUpdate.txt","w")
+txtFile.write (arcpy.GetMessages())
+txtFile.close()		
+
